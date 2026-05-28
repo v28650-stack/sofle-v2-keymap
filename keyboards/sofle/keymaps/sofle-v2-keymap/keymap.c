6b12d6377674971651563da3785dc074ac48c445
v28650-stack <v28650@gmail.com>
2026-05-28 12:26:42 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/sofle-v2-keymap/keymap.c
+++ keyboards/sofle/keymaps/sofle-v2-keymap/keymap.c
@@ -4,7 +4,7 @@
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_left()) {
-        return OLED_ROTATION_270;
+        return OLED_ROTATION_0;
     } else {
         return OLED_ROTATION_180;
     }

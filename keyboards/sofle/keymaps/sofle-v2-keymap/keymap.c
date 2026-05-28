d2994cae17d61fb61476dbca432e9e59ed400b02
v28650-stack <v28650@gmail.com>
2026-05-28 11:44:54 -0400

    Update keymap.c

--- keyboards/sofle/keymaps/sofle-v2-keymap/keymap.c
+++ keyboards/sofle/keymaps/sofle-v2-keymap/keymap.c
@@ -6,13 +6,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
     }
-    return OLED_ROTATION_180;
-}
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
     return OLED_ROTATION_0;
 }
 

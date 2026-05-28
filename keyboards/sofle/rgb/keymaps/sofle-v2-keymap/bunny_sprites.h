55aaf500b3297269d282c9d654ceb1fbf7ad1ad3
v28650-stack <v28650@gmail.com>
2026-05-28 14:32:41 -0400

    Create bunny_sprites.h

---
+++ keyboards/sofle/rgb/keymaps/sofle-v2-keymap/bunny_sprites.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+// 8 frames (PLACEHOLDER SAFE STRUCTURE)
+
+static const char bunny_frame_0[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_1[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_2[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_3[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_4[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_5[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_6[] PROGMEM = { /* 128 bytes */ };
+static const char bunny_frame_7[] PROGMEM = { /* 128 bytes */ };

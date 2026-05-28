8281003183b0dd52be434e9ffe21cd9dcaaa32eb
v28650-stack <v28650@gmail.com>
2026-05-28 14:05:52 -0400

    Create bunny_sprites.h

---
+++ layouts/keyboards/sofle/rgb/keymaps/sofle-v2-keymap/bunny_sprites.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+#define SPRITE_WIDTH 128
+#define SPRITE_HEIGHT 32
+#define SPRITE_SIZE (SPRITE_WIDTH * SPRITE_HEIGHT / 8)
+
+static const unsigned char PROGMEM bunny_frame_0[] = { /* frame 0 data */ };
+static const unsigned char PROGMEM bunny_frame_1[] = { /* frame 1 data */ };
+static const unsigned char PROGMEM bunny_frame_2[] = { /* frame 2 data */ };
+static const unsigned char PROGMEM bunny_frame_3[] = { /* frame 3 data */ };
+static const unsigned char PROGMEM bunny_frame_4[] = { /* frame 4 data */ };
+static const unsigned char PROGMEM bunny_frame_5[] = { /* frame 5 data */ };
+static const unsigned char PROGMEM bunny_frame_6[] = { /* frame 6 data */ };
+static const unsigned char PROGMEM bunny_frame_7[] = { /* frame 7 data */ };
